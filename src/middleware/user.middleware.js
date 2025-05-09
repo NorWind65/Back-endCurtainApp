2a5dab52c0ddf325422cf2c021fc9df404d50e80
NorWind65 <viet.hq210936@sis.hust.edu.vn>
2025-05-09 11:38:42 +0700

    fix decode

--- src/middleware/user.middleware.js
+++ src/middleware/user.middleware.js
@@ -12,9 +12,9 @@ const protectUserRoute = async (req, res, next) => {
         }
         //verify token
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const user = await User.findById(decoded.userId).select("-password");
+        const user = await User.findById(decoded.id).select("-password");
         if (!user) {
-            return res.status(401).json({ message: "Token is not valid" ,token});
+            return res.status(401).json({ message: "Token is not valid" });
         }
 
         req.user = user;
